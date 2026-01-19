43874aec2934243d302bfc50e5ddfb8be0202f43
Hary Chandra Mahardika <chandramahardika82@gmail.com>
2026-01-19 15:24:46 +0700

    first commit

--- api/[...all].jsx
+++ api/[...all].jsx
@@ -2,27 +2,20 @@ import axios from "axios";
 
 export default async function handler(req, res) {
   try {
-    // Backend Laravel VPS HTTP
     const backendBase = "http://203.194.115.210:8008/api";
-
-    // req.query.all adalah array path, gabungkan jadi string
     const path = req.query.all.join("/");
-
     const url = `${backendBase}/${path}`;
 
     const response = await axios({
       method: req.method,
       url,
-      headers: {
-        ...req.headers,
-        host: undefined, // hapus header host lama
-      },
+      headers: { ...req.headers, host: undefined },
       data: req.body,
     });
 
     res.status(response.status).json(response.data);
   } catch (err) {
-    console.error(err.message);
+    console.error(err);
     res.status(err.response?.status || 500).json({
       message: err.message,
       data: err.response?.data || null,
